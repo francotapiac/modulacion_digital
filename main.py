d0bbd2adc076374316d8db2ce4d9684b0af679e6
franco tapia <franco.tapia.c@usach.cl>
2020-09-05 20:14:00 -0400

    Demodulación sin terminar

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io.wavfile as sc
-import warnings
-warnings.filterwarnings('ignore')
+import scipy as sc
+from scipy import integrate
 
 #Cada vez que aumenta la tasa de datos (tasa de bits), debe aumentar la frecuencia de la portadora
 
@@ -11,15 +10,16 @@ A = 1                                               #Amplitud con bit 0
 B = 2                                               #Amplitud con bit 1
 bps = 10                                            #tasa de bits: bits por segundo
 tiempoArreglo = len(arregloBits)                    #Tiempo duración
-frecuenciaMuestrePortadora = 15
+frecuenciaPortadora = 10*bps
+frecuenciaMuestreoPortadora = 10*frecuenciaPortadora
 
 ############### Todo lo relacionado a la portadora (mover a otro archivo)
-def obtenerTiempoPortadora(bps):
-    tiempoPortadora = np.linspace(0,1 - 1/bps,bps)
+def obtenerTiempoPortadora(bps,frecuenciaMuestreoPortadora):
+    tiempoPortadora = np.linspace(0,1/bps,frecuenciaMuestreoPortadora)    #Se multiplica
     return tiempoPortadora
 
-def obtenerPortadora(frecuenciaMuestrePortadora,tiempoPortadora):
-    portadora = np.cos(2*np.pi*frecuenciaMuestrePortadora*tiempoPortadora)
+def obtenerPortadora(frecuenciaPortadora,tiempoPortadora):
+    portadora = np.cos(2*np.pi*frecuenciaPortadora*tiempoPortadora)
     return portadora
 
 ############### Todo lo relacionado a modular ASK (mover a otro archivo)
@@ -30,8 +30,42 @@ def moduladorASK(A,B,arregloBits, tiempoArreglo, portadora):
             modulador = np.concatenate([modulador,(A*portadora)])   
         else:
             modulador = np.concatenate([modulador,(B*portadora)])    
-    tiempoModulador = np.linspace(0,tiempoArreglo,len(modulador))
+    tiempoModulador = np.linspace(0,tiempoArreglo/bps,len(modulador))
     return modulador,tiempoModulador
+
+
+########### Demodular
+def calcularArea(y,x):
+    integral = integrate.trapz(y,x)
+    return integral
+
+def demodularASK(modulada, portadora, tamanoSegmento, tiempoPortadora,tiempoModulada):
+    areaA1 = calcularArea(portadora,tiempoPortadora)
+    print("areaA1:" + str(areaA1))
+    demodulada = []
+    segmento = []
+    segmentoTiempo = []
+    contador = 0
+    areaSegmento = 0
+
+    for elemento1, elemento2 in zip(modulada,tiempoModulada):
+        if( contador == tamanoSegmento - 1):
+            areaSegmento = calcularArea(segmento,segmentoTiempo)
+            print("area segmento:" + str(areaSegmento))
+            if( areaSegmento >= areaA1):
+                demodulada.append(1)
+            else:
+                demodulada.append(0)
+            segmentoTiempo = []
+            segmento = []
+            contador = 0
+        else:
+            segmento.append(elemento1)
+            segmentoTiempo.append(elemento2)
+            contador = contador + 1
+    print(demodulada)
+    return demodulada
+
 
 
 
@@ -50,19 +84,20 @@ def crearSubGrafico(dato,valorEjeX,titulo,xLabel,yLabel,color,filas,columnas,pos
     plt.ylabel(yLabel)
 
 ############### Main ###############
-tiempoPortadora = obtenerTiempoPortadora(bps)
-portadora = obtenerPortadora(frecuenciaMuestrePortadora,tiempoPortadora)
+tiempoPortadora = obtenerTiempoPortadora(bps,frecuenciaMuestreoPortadora)
+portadora = obtenerPortadora(frecuenciaPortadora,tiempoPortadora)
 
-modulador,tiempoModulador = moduladorASK(A,B,arregloBits,tiempoArreglo,portadora)
+modulada,tiempoModulador = moduladorASK(A,B,arregloBits,tiempoArreglo,portadora)
+demodulada = demodularASK(modulada, portadora, frecuenciaMuestreoPortadora,tiempoPortadora,tiempoModulador)
 
-
-crearSubGrafico(arregloBits,np.linspace(0,tiempoArreglo,tiempoArreglo),"Arreglo de bits","tiempo","amplitud","r",3,1,1)
+crearSubGrafico(arregloBits,np.linspace(0,tiempoArreglo/bps,tiempoArreglo),"Arreglo de bits","tiempo","amplitud","r",3,1,1)
 crearSubGrafico(portadora,tiempoPortadora,"Portadora en el tiempo","tiempo","amplitud","g",3,1,2)
-crearSubGrafico(modulador,tiempoModulador,"Modulada ASK","tiempo","amplitud","purple",3,1,3)
+crearSubGrafico(modulada,tiempoModulador,"Modulada ASK","tiempo","amplitud","purple",3,1,3)
 plt.show()
 
 graficar(portadora,tiempoPortadora,"Señal Portadora")
-graficar(modulador,tiempoModulador,"Modulada ASK")
+graficar(modulada,tiempoModulador,"Modulada ASK")
+#graficar(demodulada,tiempoArreglo,"Demodulada ASK")
 
 
 
