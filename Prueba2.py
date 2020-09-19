c6cf0fe175a37e9c562359e4d2ca09df29b0bd4c
sebastian orellana v <sebastian.orellana.v@usach.cl>
2020-09-19 19:58:18 -0300

    se arregla simulacion

--- Prueba2.py
+++ Prueba2.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 from scipy import integrate
+from numpy.random import rand, randn
 
 def generarSenal(largo):
     senal = np.random.randint(2,size = largo)
@@ -23,13 +24,14 @@ def graficarSenalDigital(senal, tiempo, titulo, xLabel, yLabel):
     plt.ylabel(yLabel)
     plt.grid()
 
-def modularASK(senal, bps, freqPortadora, freqMuestreoPortadora):
+def modularASK(senal, bps, freqPortadora, freqMuestreoPortadora, cond):
     #Generar Intervalo de tiempo para la portadora
     tiempoPortadora = np.linspace(0,1/bps,freqMuestreoPortadora)
     #Generar Portadora
     portadora = np.cos(2*np.pi*freqPortadora*tiempoPortadora)
     #Graficar Portadora en el tiempo
-    graficar(portadora, tiempoPortadora, "Señal Portadora en el tiempo", "Tiempo (s)", "Amplitud")
+    if(cond):
+        graficar(portadora, tiempoPortadora, "Señal Portadora en el tiempo", "Tiempo (s)", "Amplitud")
 
     #Algoritmo para modular
     senalModulada = []
@@ -42,16 +44,25 @@ def modularASK(senal, bps, freqPortadora, freqMuestreoPortadora):
     #Generar intervalo de tiempo para la modulada
 
 def simuladorRuido(senal, tiempoModulada, snrDb):
-    ruido = np.random.normal(0.0, snrDb, len(senal))
-    senalRuido = modulada + ruido
+    w = 1
+    T = 2*np.pi/w
+    energia = sc.integrate.simps(modulada**2,tiempoModulada)
+    potenciaSenal = (1/T)*energia
+    print(potenciaSenal)
+    snr = 10.0**(snrDb/10.0)
+    print(snr)
+    desviacionEstandar = np.sqrt(potenciaSenal/snr)
+    print(desviacionEstandar)
+    ruido = np.random.normal(0,desviacionEstandar,len(senal))
+    senalRuido = senal + ruido
     return senalRuido
 
-def demodularASK(modulada):
+def demodularASK(modulada, bps,freqPortadora, freqMuestreoPortadora):
     #Generar Intervalo de tiempo para la portadora
     tiempoPortadora = np.linspace(0,1/bps,freqMuestreoPortadora)
     #Generar Portadora
     portadora = np.cos(2*np.pi*freqPortadora*tiempoPortadora)
-    areaEstandar = integrate.trapz(2*portadora*portadora, tiempoPortadora)
+    areaEstandar = integrate.trapz(3*portadora*portadora, tiempoPortadora)
     inicio = 0
     fin = len(tiempoPortadora)
     demodulada = []
@@ -68,31 +79,100 @@ def demodularASK(modulada):
     print(np.array(demodulada))
     return demodulada
 
+def calcularBER(senalOriginal,senalDemodulada):
+    cantidadDiferentes = 0
+    for elemento1,elemento2 in zip(senalOriginal,senalDemodulada):
+        if(elemento1 != elemento2):
+            cantidadDiferentes = cantidadDiferentes + 1
+    ber = cantidadDiferentes/len(senalOriginal)
+    return ber
+
+def simulacion(modulada, tiempoModulada, arraySNRinDb, freqPortadora, freqMuestreoPortadora):
+    #array para guardar los ber calculados para cada señal con diferentes snr
+    moduladaRuido = []
+    demodulada = []
+    BER = [None]*len(arraySNRinDb)
+    i = 0
+    for snrDb in arraySNRinDb:
+        ber = 0
+        #Agregar Ruido
+        moduladaRuido = simuladorRuido(modulada, tiempoModulada, snrDb)
+        #graficar(moduladaRuido, tiempoModulada, "Señal Modulada con Ruido AWGN - SNR="+str(snrDb) + " BPS="+str(bps), "Tiempo (s)", "Amplitud")
+        #Demodular la Señal
+        demodulada = demodularASK(moduladaRuido, bps, freqPortadora, freqMuestreoPortadora)
+        #Calcular BER
+        ber = calcularBER(senalDigital, demodulada)
+        print("ber: "+ str(ber))
+        #Guardar ber
+        BER[i] = ber
+        i +=1
+    return BER
+
 ########################################################################################################
 #Datos Iniciales
+largoSenal = 10
 bps = 5
 freqPortadora = 3*bps
 freqMuestreoPortadora = 5*freqPortadora
-arregloSNR = [-2,-1,0.5,1]
-arregloBPS = [5,10,15]
 snrDb = 0.1
 
 #Prueba para un caso pequeño
 #Generar Señal
-senalDigital = generarSenal(100)
-print("senal original")
+senalDigital = generarSenal(10)
+print("senal corta")
 print(senalDigital)
 tiempo = np.linspace(0,len(senalDigital)/bps,len(senalDigital))
 graficarSenalDigital(senalDigital, tiempo, "Señal Digital en el Tiempo", "Tiempo (s)", "Amplitud")
 #Modular la Señal
-modulada, tiempoModulada = modularASK(senalDigital, bps, freqPortadora, freqMuestreoPortadora)
+modulada, tiempoModulada = modularASK(senalDigital, bps, freqPortadora, freqMuestreoPortadora, True)
 graficar(modulada, tiempoModulada, "Señal Modulada en ASK", "Tiempo (s)", "Amplitud")
 #Agregar Ruido
 moduladaRuido = simuladorRuido(modulada, tiempoModulada, snrDb)
 graficar(moduladaRuido, tiempoModulada, "Señal Modulada con Ruido AWGN", "Tiempo (s)", "Amplitud")
 #Demodular la Señal
-demodulada = demodularASK(moduladaRuido)
+demodulada = demodularASK(moduladaRuido, bps, freqPortadora, freqMuestreoPortadora)
 
 #Simulacion Caso grande
+largoSenal = 10000
+arraySNRinDb = range(0,5)
+arrayBPS = [5,10,20]
+plt.figure()
+for bps in arrayBPS:
+    freqPortadora = 3*bps
+    freqMuestreoPortadora = 5*freqPortadora
+    senalDigital = generarSenal(largoSenal)
+    modulada, tiempoModulada = modularASK(senalDigital, bps, freqPortadora, freqMuestreoPortadora, False)
+    BER = simulacion(modulada, tiempoModulada, arraySNRinDb, freqPortadora, freqMuestreoPortadora)
+
+    plt.plot(arraySNRinDb, BER, 'bo', arraySNRinDb, BER, 'k')
+    #plt.axis([0, 10, 1e-6, 0.1])
+    plt.xscale('linear')
+    plt.yscale('log')
+    plt.xlabel('SNR(dB)')
+    plt.ylabel('BER')
+    plt.grid(True)
+    plt.title('BER vs SNR')
+
+"""
+for n in range (0, lenSNR): 
+    snrDb = arraySNRinDb[n]   
+    snr=10.0**(snrDb/10.0)
+    ruido = 1/np.sqrt(2*snr)
+    SenalRuido = modulada + ruido * randn(len(modulada))
+    demodulada = demodularASK(SenalRuido, bps, freqPortadora, freqMuestreoPortadora)
+    BER[n] = calcularBER(senalDigital, demodulada)
+"""
+
 
 plt.show()
+
+"""
+print(resultadoSimulacion[0])
+print(resultadoSimulacion[1])
+print(resultadoSimulacion[2])
+plt.figure()
+plt.plot(SNR,resultadoSimulacion[0])
+plt.plot(SNR,resultadoSimulacion[1])
+plt.plot(SNR,resultadoSimulacion[2])
+plt.show()
+"""
