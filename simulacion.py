798db49bc7a3b08e7168be2ac88c1d0a8ffc9d08
sebastian orellana v <sebastian.orellana.v@usach.cl>
2020-09-20 19:09:48 -0300

    se comenta el simulacion.py

--- simulacion.py
+++ simulacion.py
@@ -2,6 +2,15 @@ import numpy as np
 import modulacion as mod
 import graficar as graf
 import scipy as sc
+
+# Entradas: senal -> array con los datos de la señal
+#           tiempoModulada -> array que representa el tiempo
+#           snrdB -> entero que representa el snr en decibeles
+#           cond -> booleano que se utiliza para saber si se debe graficar o no
+# Salidas:   senalRuido -> array con los datos de la señal con ruido
+#------------------------------------------
+# Esta funcion agrega ruido blanco gaussiano a la señal ingresada como 
+# parametro segun el snr ingresado
 def simuladorRuido(senal, tiempoModulada, snrDb, cond):
     w = 1
     T = 2*np.pi/w
@@ -18,6 +27,12 @@ def simuladorRuido(senal, tiempoModulada, snrDb, cond):
     senalRuido = senal + ruido
     return senalRuido
 
+# Entradas: senalOriginal -> array con los datos de la señal
+#           senalDemodulada -> array con los datos de la señal demodulada
+# Salidas:   ber -> float que representa la tasa de error de bits
+#------------------------------------------
+# Esta funcion cuenta los bits diferentes entre la señal original 
+# y la señal demodulada y calcula la tasa de error de bits
 def calcularBER(senalOriginal,senalDemodulada):
     cantidadDiferentes = 0
     for elemento1,elemento2 in zip(senalOriginal,senalDemodulada):
@@ -26,6 +41,18 @@ def calcularBER(senalOriginal,senalDemodulada):
     ber = cantidadDiferentes/len(senalOriginal)
     return ber
 
+# Entradas: senalOriginal -> array con los datos de la señal
+#           modulada -> array con los datos de la señal modulada
+#           tiempoModulada -> array que representa el tiempo
+#           arraySNRinDb -> array que contiene los snr a utilizar
+#           freqPortadora -> frecuencia de la señal portadora utilizada
+#           freqMuestreoPortadora -> frecuencia de muestreo de la señal portadora utilizada
+#           bps -> Tasa de bits por segundo utilizada
+# Salidas:   BER -> array con los ber calculados
+#------------------------------------------
+# Esta funcion agrega diferentes niveles de ruido blanco gaussiano a la señal 
+# ingresada como parametro luego demodula y va calculando las tasas de error de bits
+# los ber calculados se guardan y se retornan en un array
 def simulacion(senalOriginal, modulada, tiempoModulada, arraySNRinDb, freqPortadora, freqMuestreoPortadora, bps):
     #array para guardar los ber calculados para cada señal con diferentes snr
     moduladaRuido = []
